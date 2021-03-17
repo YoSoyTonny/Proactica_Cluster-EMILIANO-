f550b0724ddad3fc2cc2679c01bd5c0e3d323307
Antonio <itonygame@gmail.com>
2021-03-16 17:09:27 -0700

    Practica Cluster

---
+++ proceso-master.js
@@ -0,0 +1,20 @@
+const cluster = require('cluster');
+console.log(`Master pid: ${process.pid}`);
+cluster.setupMaster({
+exec : __dirname + '/servicio-recetas.js'
+});
+
+cluster.fork();
+cluster.fork();
+
+cluster
+    .on('disconnect', (worker)=> {
+        console.log(`Desconectado: ${worker.id}` )
+    })
+    .on('exit', (worker, code, signal) => {
+        console.log(`Salida: ${worker.id}`);
+        cluster.fork();
+    })
+    .on('listening', (worker, {address, port}) => {
+        console.log(`Escuchando ${worker.id} en ${address}:${port}`)
+    });
